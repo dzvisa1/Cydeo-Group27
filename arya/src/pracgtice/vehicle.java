185afbdf6c74f230f9ccdf823c636f9aa6a7450e
dzvisa1 <dzvisaa@gmail.com>
2022-08-23 21:02:35 -0400

    new class created please add calling

---
+++ arya/src/pracgtice/vehicle.java
@@ -0,0 +1,14 @@
+package pracgtice;
+
+public class vehicle {
+    String brand;
+    int year;
+    double price;
+    long vinNumber;
+    public void Car (String brand, int year, double price, long vinNumber){
+        this.brand=brand;
+        this.year=year;
+        this.price=price;
+        this.vinNumber=vinNumber;
+    }
+}
